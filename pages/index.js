f480150b73490c5c36d45b5fcdcfbd5e2bb8a41c
damian plastina <damianplastina14@gmail.com>
2020-09-03 00:46:45 -0300

    crea proyecto nextjs

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Pruebas react</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <h1>
+          Bienvenido al proyecto de prueba de react
+        </h1>
+      </main>
+    </div>
+  )
+}

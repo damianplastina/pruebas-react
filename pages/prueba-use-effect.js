9ce5de93fec3e41fbd72a8ca18f1bc0f6136e7a5
damian plastina <damianplastina14@gmail.com>
2020-09-03 00:48:58 -0300

    prueba de useeffect

---
+++ pages/prueba-use-effect.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+
+// MAS INFORMACIÓN DE USE EFFECT
+// https://es.reactjs.org/docs/hooks-effect.html
+export default function Home() {
+
+    const [contador, setContador] = useState(0);
+
+    console.log("fuera de useeffect")
+
+    useEffect(() => {
+
+        console.log("se ejecuta luego de cada renderizado")
+    });
+
+    useEffect(() => {
+
+        console.log("se ejecuta sólo una vez")
+    }, []);
+
+    useEffect(() => {
+
+        console.log("se ejecuta si hubo un cambio en count ")
+    }, [contador]);
+
+    return (
+        <div>
+            <main>
+                <h1>
+                    Contador: {contador}
+                </h1>
+                <button
+                    onClick={() => {
+                        setContador(contador + 1)
+                    }}>
+                    incrementar contador
+                </button>
+                <div>Mirar la consola</div>
+            </main>
+        </div>
+    )
+}
